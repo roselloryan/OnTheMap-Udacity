cd6e2182e787b030bc485118d2d021596710974f
Ryan Rosello <roselloryan@gmail.com>
2017-10-03 13:11:37 -0700

    created ui for iphone login view controller

---
+++ OnTheMap/Controllers/LoginViewController.swift
@@ -0,0 +1,101 @@
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addGradientToView()
+        addTapGestureToView()
+        
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        
+        // Set placeholder texts attributes
+        emailTextField.attributedPlaceholder = emailAttributedString()
+        passwordTextField.attributedPlaceholder = passwordAttributedString()
+    
+    }
+    
+    @objc func resignAnyFirstResponder() {
+        emailTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+    }
+
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+    
+    }
+    
+    @IBAction func signUpButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Placeholder attributed string methods
+    func emailAttributedString() -> NSAttributedString {
+
+        return NSAttributedString(string: "Email", attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 17), NSAttributedStringKey.foregroundColor: UIColor.white])
+    }
+    
+    func passwordAttributedString() -> NSAttributedString {
+        
+        return NSAttributedString(string: "Password", attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 17), NSAttributedStringKey.foregroundColor: UIColor.white])
+    }
+    
+    // MARK: Gradient method
+    func addGradientToView() {
+        let gLayer = CAGradientLayer()
+        gLayer.frame = view.bounds
+        gLayer.colors = [UIColor(red: 255/255, green: 150/255, blue: 0, alpha: 1).cgColor, UIColor(red: 255/255, green: 95/255, blue: 0, alpha: 1).cgColor]
+        view.layer.insertSublayer(gLayer, at: 0)
+    }
+    
+    // MARK: Tap Gesture Method
+    func addTapGestureToView() {
+        let tapRecognizer = UITapGestureRecognizer.init(target: self, action: #selector(resignAnyFirstResponder))
+        view.addGestureRecognizer(tapRecognizer)
+    }
+
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.attributedPlaceholder = nil
+        
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextFieldDidEndEditingReason) {
+        if textField.text == "" {
+            textField.attributedPlaceholder = textField == emailTextField ? emailAttributedString() : passwordAttributedString()
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        return true
+    }
+}
---
+++ OnTheMap/Models/Constants.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+struct Constants {
+    
+    struct HeaderKeys {
+        static let AppID = "X-Parse-Application-Id"
+        static let APIKey = "X-Parse-REST-API-Key"
+        static let ContentType = "Content-Type"
+    }
+
+    struct HeaderValues {
+        static let AppId = "QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr"
+        static let APIKey = "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"
+        static let Json = "application/json"
+    }
+
+    struct Url {
+        static let ApiScheme = "https"
+        static let ApiHost = "https://parse.udacity.com"
+        static let ApiPath = "/parse/classes"
+        static let UdacitySessionUrl = "https://www.udacity.com/api/session"
+        
+    }
+}
