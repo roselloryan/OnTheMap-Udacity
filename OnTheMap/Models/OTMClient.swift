1f564c084f40f52aed637bfb77aa90e3058195aa
Ryan Rosello <roselloryan@gmail.com>
2017-10-04 09:41:09 -0700

    finished login UI for iPhone and iPad

---
+++ OnTheMap/Models/OTMClient.swift
@@ -0,0 +1,13 @@
+//
+//  OTMClient.swift
+//  OnTheMap
+//
+//  Created by RYAN ROSELLO on 10/4/17.
+//  Copyright © 2017 RYAN ROSELLO. All rights reserved.
+//
+
+import UIKit
+
+class OTMClient: NSObject {
+
+}
---
+++ OnTheMap/Views/GradientView.swift
@@ -0,0 +1,21 @@
+//
+//  GradientView.swift
+//  OnTheMap
+//
+//  Created by RYAN ROSELLO on 10/3/17.
+//  Copyright © 2017 RYAN ROSELLO. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
